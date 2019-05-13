3fb7f87193b1a3012186b069d56fe11d12aa9379
yamachu <yamachu.dev@gmail.com>
2019-05-13 10:44:58 +0900

    休日とそれ以外でul要素の数が変わるので、最後のNodeを使用する

--- src/query.ts
+++ src/query.ts
@@ -2,13 +2,13 @@ export const LOGIN_EMAIL = '#user_email';
 export const LOGIN_PASSWORD = '#user_password';
 export const LOGIN_BUTTON = '#new_user > div > ul > li > input';
 
-export const LIST_DATES = '//*[@id="top"]/div/main/div/ul[2]/li[*]/section/h2/span';
+export const LIST_DATES = '//*[@id="top"]/div/main/div/ul[last()]/li[*]/section/h2/span';
 export const LIST_MENU_NAMES =
-    '//*[@id="top"]/div/main/div/ul[2]/li[*]/section/ul/li[1]/section/div/h3';
+    '//*[@id="top"]/div/main/div/ul[last()]/li[*]/section/ul/li[1]/section/div/h3';
 export const LIST_ORDER_BUTTONS =
-    '//*[@id="top"]/div/main/div/ul[2]/li[*]/section/ul/li[1]/section/div/form/input[@name="commit"]';
+    '//*[@id="top"]/div/main/div/ul[last()]/li[*]/section/ul/li[1]/section/div/form/input[@name="commit"]';
 export const LIST_ORDER_BUTTON = (_: any, ...args: any[]) =>
-    `//*[@id="top"]/div/main/div/ul[2]/li[${
+    `//*[@id="top"]/div/main/div/ul[last()]/li[${
         args[0]
     }]/section/ul/li[1]/section/div/form/input[@name="commit"]`;
 
